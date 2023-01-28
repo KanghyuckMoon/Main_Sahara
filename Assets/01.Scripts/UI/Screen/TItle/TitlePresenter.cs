9bc75466d0cd28c3ebbb17b18de0c654b5e35e43
pilgyu14 <pilgue0822@naver.com>
2023-01-28 15:56:55 +0900

    0128_ui 스크립트 초기화부분 수정

---
+++ Assets/01.Scripts/UI/Screen/TItle/TitlePresenter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class TitlePresenter : MonoBehaviour
+    {
+        private TitleView titleView; 
+
+    }
+
+}
