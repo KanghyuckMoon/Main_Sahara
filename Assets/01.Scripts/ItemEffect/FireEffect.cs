b48437acb8a1fe7e8cbfe9f84dc7be7e1d34475f
moonkanghyuck <moonkanghyuck@gmail.com>
2023-02-17 13:20:13 +0900

    0217_악세사리&이펙트_불이펙트생성 악세사리 추가

---
+++ Assets/01.Scripts/ItemEffect/FireEffect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEffect : MonoBehaviour
+{
+	[SerializeField]
+	private ParticleSystem[] particleSystemArray;
+
+	public void OnEnable()
+	{
+		MeshRenderer _parentRenderer = transform.GetComponentInParent<MeshRenderer>();
+
+		if (_parentRenderer is null)
+		{
+			return;
+		}
+
+		foreach (var _particle in particleSystemArray)
+		{
+			var _shape = _particle.shape;
+			_shape.meshRenderer = _parentRenderer;
+		}
+	}
+}
