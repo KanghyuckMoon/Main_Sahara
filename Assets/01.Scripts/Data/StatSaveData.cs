6091be0ccb87c5f63168f0e35638e9d9f19a8a9e
bossxz <moonkanghyuck@gmail.com>
2023-02-24 23:57:50 +0900

    0224_오브젝트 데이터 스탯 데이터 연동 테스트

---
+++ Assets/01.Scripts/Data/StatSaveData.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utill.Addressable;
+using Utill.Pattern;
+using ForTheTest;
+
+namespace Data
+{
+    public class StatSaveData
+    {
+        public int maxHp;
+        public int currentHp;
+        public int maxMana;
+        public int currentMana;
+        public int meleeAttack;
+        public int rangeAttack;
+        public int magicAttack;
+
+        public float speed;
+        public float jump;
+
+        public void Copy(StatData statData)
+		{
+            maxHp = statData.MaxHp;
+            currentHp = statData.CurrentHp;
+            maxMana = statData.MaxMana;
+            currentMana = statData.CurrentMana;
+            meleeAttack = statData.MeleeAttack;
+            rangeAttack = statData.RangeAttack;
+            magicAttack = statData.MagicAttack;
+            speed = statData.Speed;
+            jump = statData.Jump;
+        }
+    }
+}
