d98694b2ecd638c02f01d1ad4facb62468d0f6a1
meng-joo <81345410+meng-joo@users.noreply.github.com>
2023-03-08 01:24:52 +0900

    0308_버프_ 버프&디버프 완성

---
+++ Assets/01.Scripts/Module/BuffEffect/Healing_Buf.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Module;
+
+namespace Buff
+{
+    public class Healing_Buf : AbBuffEffect
+    {
+        private HpModule hpModule;
+
+        private float currentDuration;
+        private float currentPeriod;
+
+        public Healing_Buf(BuffModule _buffModule) : base(_buffModule)
+        {
+            currentDuration = duration;
+        }
+
+        public override void Buff(AbMainModule _mainModule)
+        {
+            hpModule ??= _mainModule.GetModuleComponent<HpModule>(ModuleType.Hp);
+            Heal(hpModule);
+        }
+
+        private void Heal(HpModule _hpModule)
+        {
+            if (currentDuration >= 0)
+            {
+                if (currentPeriod <= 0)
+                {
+                    hpModule.GetHeal((int)value);
+                    currentPeriod = period;
+                }
+
+                currentPeriod -= Time.deltaTime;
+                currentDuration -= Time.deltaTime;
+            }
+
+            else
+            {
+                buffModule.buffDic.Remove(this);
+            }
+        }
+    }
+}
--- Assets/01.Scripts/Module/BuffModule.cs
+++ Assets/01.Scripts/Module/BuffModule.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Buff;
 
 namespace Module
 {
+    public enum Bufftype
+    {
+        Update,
+        Once,
+        None
+    }
+
     public class BuffModule : AbBaseModule
     {
-        //public Dictionary<>
+        public Dictionary<IBuff, Bufftype> buffDic = new Dictionary<IBuff, Bufftype>();
 
         public BuffModule(AbMainModule _mainModule) : base(_mainModule)
         {
 
         }
 
-        public void AddBuff()
+        public override void Start()
         {
+            AddBuff(new Healing_Buf(this).SetValue(10).SetDuration(10).SetPeriod(2).SetSpownObjectName("HealEffect"), Bufftype.Update);
+        }
 
+        public void AddBuff(IBuff _buff, Bufftype _bufftype)
+        {
+            buffDic.Add(_buff, _bufftype);
+            if (_bufftype == Bufftype.Once)
+            {
+                _buff.Buff(mainModule);
+                buffDic.Remove(_buff);
+            }
+        }
+
+        public override void Update()
+        {
+            foreach(IBuff _buff in buffDic.Keys)
+            {
+                if (buffDic[_buff] == Bufftype.Update)
+                    _buff.Buff(mainModule);
+            }
         }
     }
 }
