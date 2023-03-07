cd60ef8c38194ce13b69c989d48c37778e2702de
meng-joo <81345410+meng-joo@users.noreply.github.com>
2023-03-08 00:00:57 +0900

    0308_버프디버프_기반

---
+++ Assets/01.Scripts/Buff/BuffEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buff
+{
+    public class BuffEffect : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/01.Scripts/Module/BuffModule.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Module
+{
+    public class BuffModule : AbBaseModule
+    {
+        //public Dictionary<>
+
+        public BuffModule(AbMainModule _mainModule) : base(_mainModule)
+        {
+
+        }
+
+        public void AddBuff()
+        {
+
+        }
+    }
+}
