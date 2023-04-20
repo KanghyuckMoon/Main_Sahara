dff27b2b2259d4dedd4e1579da34cb834613ed81
meng-joo <81345410+meng-joo@users.noreply.github.com>
2023-04-20 10:11:39 +0900

    0420_플레이어_착지 사운드

--- Assets/AttackAnimation.cs
+++ Assets/AttackAnimation.cs
@@ -19,6 +19,7 @@ public class AttackAnimation : StateMachineBehaviour
         mainModule.StopOrNot = 0;
 
         mainModule.SetConsecutiveAttack(0);
+        mainModule.SetActiveAnimatorRoot(1);
 
         stateModule.AddState(State.ATTACK);
 
