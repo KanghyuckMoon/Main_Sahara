8a2d34c87bcfe148393dd1f984340381bf80e2da
StraightBet <slupgames1025@gmail.com>
2023-05-09 19:09:09 +0900

    0509_아이템 데이터들 카운트 1씩 추가

--- Assets/01.Scripts/Inventory/InventoryManager.cs
+++ Assets/01.Scripts/Inventory/InventoryManager.cs
@@ -305,11 +305,13 @@ namespace Inventory
 					else
 					{
 						inventorySO.itemDataList.Add(_itemData);
+						SendEvent("QuestManager", null);
 						SendEvent("PopupUIManager", _itemData);
 						break;
 					}
 
 					inventorySO.itemDataList.Add(_itemData);
+					SendEvent("QuestManager", null);
 					SendEvent("PopupUIManager", _itemData);
 				}
 			}
