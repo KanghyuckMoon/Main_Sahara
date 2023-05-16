2841d4b32ccacecaecdde6090ecd67e78f7b34a2
pilgyu14 <pilgue0822@naver.com>
2023-05-16 15:28:25 +0900

    0516_추가

---
+++ Assets/01.Scripts/UI/Popup/UIParticleDataSO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UI.ParticleManger;
+using UnityEngine;
+
+namespace  UI.Popup
+{
+    [System.Serializable]
+    public class UIParticleData
+    {
+        public ParticleType particleType;
+        public string particleAdress;
+
+    }
+    public class UIParticleDataSO
+    {
+        
+    }
+
+}
+
