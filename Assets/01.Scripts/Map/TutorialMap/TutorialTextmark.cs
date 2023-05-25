91472ff68c4ced2f65664ce275382317a2b8132d
moonkanghyuck <moonkanghyuck@gmail.com>
2023-05-25 20:15:28 +0900

    0525_튜토리얼 맵 텍스트 관련 작업중

---
+++ Assets/01.Scripts/Map/TutorialMap/TutorialTextmark.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map
+{
+    public class TutorialTextmark : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+        
+        }
+    }   
+}
