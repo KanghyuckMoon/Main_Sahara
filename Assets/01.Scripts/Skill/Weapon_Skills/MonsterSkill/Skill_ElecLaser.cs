83766df5baed999fa26979c105bf8c09b4622a14
moonkanghyuck <moonkanghyuck@gmail.com>
2023-05-30 22:38:35 +0900

    0530_네메아 제작중

--- Assets/01.Scripts/Skill/Weapon_Skills/MonsterSkill/Skill_ElecLaser.cs
+++ Assets/01.Scripts/Skill/Weapon_Skills/MonsterSkill/Skill_ElecLaser.cs
@@ -15,10 +15,13 @@ namespace Skill
         private AnimationClip animationClip;
 
         [SerializeField] private GameObject elecLaser;
-
+        [SerializeField] private Transform hitPoint;
+        [SerializeField] private Vector3 originHitPos;
+        
         public void Skills(AbMainModule _mainModule)
         {
             PlaySkillAnimation(_mainModule, animationClip);
+            hitPoint.localPosition = originHitPos;
             elecLaser.SetActive(true);
         }
         public HitBoxAction GetHitBoxAction()
