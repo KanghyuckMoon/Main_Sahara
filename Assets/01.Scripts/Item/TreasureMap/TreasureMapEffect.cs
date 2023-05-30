a4335a0394938252cc83d9e8ba1390a2441d2b5c
meng-joo <81345410+meng-joo@users.noreply.github.com>
2023-05-31 01:12:26 +0900

    0531_보물지도_퀘스트 설정

---
+++ Assets/01.Scripts/Item/TreasureMap/TreasureMapEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyNamespace
+{
+    public class TreasureMapEffect : MonoBehaviour
+    {
+        public void SetActiveActionTreasureMap()
+        {
+            
+        }
+    }
+}
