a7fef74efe85d5e8298ba1cf0cab9f9f9436d09b
moonkanghyuck <moonkanghyuck@gmail.com>
2023-06-13 10:47:49 +0900

    0613_텔레포트 관련 작업 1차

--- Assets/01.Scripts/Interaction/Event/TeleportInteraction.cs
+++ Assets/01.Scripts/Interaction/Event/TeleportInteraction.cs
@@ -1,109 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Module;
-using Effect;
-using DG.Tweening;
 
 namespace Interaction
 {
-    public class TeleportInteraction : MonoBehaviour, IInteractionItem
-    {
+        public class TeleportInteraction : MonoBehaviour, IInteractionItem
+        {
         
         
-        public bool Enabled
-        {
-            get
-            {
-                return true;
-            }
-            set
-            {
+                public bool Enabled
+                {
+                        get
+                        {
+                                return true;
+                        }
+                        set
+                        {
 
-            }
+                        }
+                }
+
+                public string Name
+                {
+                        get
+                        {
+                                return nameKey;
+                        }
+                }
+
+                public Vector3 PopUpPos
+                {
+                        get
+                        {
+                                return transform.position + new Vector3(0, 1, 0);
+                        }
+                }
+
+                public string ActionName
+                {
+                        get
+                        {
+                                return "O00000050";
+                        }
+                }
+
+                [SerializeField] private string nameKey = "M00000010";
+                [SerializeField] private TeleportSystem teleportSystem;
+                [SerializeField] private bool isUp;
+                
+                public void Interaction()
+                {
+                        teleportSystem.Interaction(isUp);
+                }
         }
-
-        public string Name
-        {
-            get
-            {
-                return nameKey;
-            }
-        }
-
-        public Vector3 PopUpPos
-        {
-            get
-            {
-                return transform.position + new Vector3(0, 1, 0);
-            }
-        }
-
-        public string ActionName
-        {
-            get
-            {
-                return "O00000050";
-            }
-        }
-
-        [SerializeField] private string nameKey = "M00000010";
-        [SerializeField] private float heightPos;
-        [SerializeField] private Transform upWall;
-        [SerializeField] private Transform downWall;
-        [SerializeField] private float upWallHeightPos;
-        [SerializeField] private float downWallHeightPos;
-
-        private Vector3 originUpWallPos;
-        private Vector3 originDownWallPos;
-        private bool isMoving = false;
-
-        void Start()
-        {
-            originUpWallPos = upWall.position;
-            originDownWallPos = downWall.position;
-        }
-
-        public void Interaction()
-        {
-            if (isMoving)
-            {
-                return;
-            }
-
-            isMoving = true;
-            
-            UpWallMoving();
-        }
-
-        private void UpWallMoving()
-        {
-            Vector3 newPosition = downWall.transform.position;
-            newPosition.y = downWallHeightPos;
-            downWall.position = newPosition;
-            upWall.DOMoveY(upWallHeightPos, 1f).OnComplete(() => PlayerTeleport());
-        }
-
-        private void PlayerTeleport()
-        {
-            Vector3 newPosition = PlayerObj.Player.transform.position;
-            newPosition.y = heightPos;
-            PlayerObj.Player.transform.position = newPosition;
-            DownWallMoving();
-        }
-
-        private void DownWallMoving()
-        {
-            Vector3 newPosition = upWall.transform.position;
-            newPosition.y = upWallHeightPos;
-            upWall.position = newPosition;
-            downWall.DOMoveY(downWallHeightPos, 5f).OnComplete(() => ResetIsMoving());
-        }
-
-        private void ResetIsMoving()
-        {
-            isMoving = false;
-        }
-    }
 }
-
