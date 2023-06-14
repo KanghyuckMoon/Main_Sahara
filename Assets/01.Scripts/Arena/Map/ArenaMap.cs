aa1e5e2637299de7a819267fa1c93a1289138e9e
pilgyu14 <pilgue0822@naver.com>
2023-06-14 19:19:03 +0900

    0614_몬스터 투기장 수정

--- Assets/01.Scripts/Arena/Map/ArenaMap.cs
+++ Assets/01.Scripts/Arena/Map/ArenaMap.cs
@@ -131,6 +131,8 @@ namespace Arena
 
         protected virtual IEnumerator Init(bool _isActive, bool _isImme = false)
         {
+            yield return new WaitForEndOfFrame();
+            Debug.Log("@@@Init");
             isActive = _isActive; 
             if (_isActive == true)
             {
