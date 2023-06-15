cf05c8d85c32147f95480b891cb4090094801331
moonkanghyuck <moonkanghyuck@gmail.com>
2023-06-15 19:21:55 +0900

    0615_삽이 여러 아이템을 동시에 퍼올릴 수 있게 됨

---
+++ Assets/01.Scripts/Detect/DigItem/ShovelDigItem.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Detect
+{
+public class ShovelDigItem : BaseDigItem
+{
+    protected List<IDetectItem> targetItemList = new List<IDetectItem>();
+    
+    protected override void GetNearObject()
+    {
+        GameObject obj = null;
+        Collider[] targets = Physics.OverlapSphere(detectTrm.position, radius,targetLayerMask);
+        foreach (Collider col in targets)
+        {
+            Vector3 dir = col.transform.position - detectTrm.position;
+            
+            var component = col.gameObject.GetComponent<IDetectItem>();
+            if ((detectItemType & component.DetectItemType) != 0)
+            {
+                targetItem = component;
+                targetItemList.Add(targetItem);   
+            }
+        }
+
+        if (targets.Length == 0)
+        {
+            targetItemList.Clear();
+        }
+    }
+
+    public override void Dig()
+    {
+        GetNearObject();
+        foreach (var _targetObj in targetItemList)
+        {
+            if(_targetObj is not null)
+            {
+                Debug.Log("GetOut");
+                _targetObj.GetOut();
+            }   
+        }
+        targetItemList.Clear();
+    }
+}
+}
+
