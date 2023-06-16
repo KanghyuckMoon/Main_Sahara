c394ea936d58fc3f8dda8c9a2e82ad48b680b693
pilgyu14 <pilgue0822@naver.com>
2023-06-17 07:35:46 +0900

    0617_보스 hud 수정

--- Assets/01.Scripts/Arena/Platform/PlatformBase.cs
+++ Assets/01.Scripts/Arena/Platform/PlatformBase.cs
@@ -8,6 +8,32 @@ using Utill.Addressable;
 
 namespace Arena
 {
+    public class CollisionEvent
+    {
+        private BoxRaycaster boxRaycaster;
+        private string targetTag; 
+        private Action triggerAction;
+        public CollisionEvent(Transform _trm, string _targetTag, Action _callback)
+        {
+            boxRaycaster = new BoxRaycaster(_trm);
+            targetTag = _targetTag;
+            triggerAction = _callback; 
+        }
+
+        public void CheckCol()
+        {
+            var _cols = boxRaycaster.MyCollisions();
+            for (int i = 0; i < _cols.Length; i++)
+            {
+                if (_cols[i].transform.CompareTag(targetTag))
+                {
+                    triggerAction?.Invoke();
+                    return; 
+                }
+            }
+        }
+    }
+
     public  class PlatformBase : MonoBehaviour, IArenaPlatform
     {
         [SerializeField] private string SOAddress; 
@@ -38,7 +64,7 @@ namespace Arena
         public virtual void StartAction()
         {
           //  float random
-            transform.DOShakePosition(0.5f, 0.1f, 5, 90f).SetLoops(-1); 
+            transform.DOShakePosition(2f, Vector3.up, 3, 90f, false, true, ShakeRandomnessMode.Harmonic).SetLoops(-1); 
         }
         
         
