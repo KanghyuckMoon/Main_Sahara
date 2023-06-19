3dc213f8788ce89bfcb9d0c79660ca87cc065634
meng-joo <81345410+meng-joo@users.noreply.github.com>
2023-06-19 23:58:46 +0900

    0619_플레이어_조작감 고치는 중 + 보.도 매복 표시

--- Assets/01.Scripts/Module/MoveModule.cs
+++ Assets/01.Scripts/Module/MoveModule.cs
@@ -128,8 +128,17 @@ namespace Module
             float _gravity = mainModule.Gravity;
             Vector3 _moveValue;
 
-            targetRotation = Mathf.Atan2(_dir.x, _dir.z) * Mathf.Rad2Deg +
-                             mainModule.ObjRotation.eulerAngles.y;
+            if (_dir != Vector3.zero)
+            {
+                targetRotation = Mathf.Atan2(_dir.x, _dir.z) * Mathf.Rad2Deg +
+                                 mainModule.ObjRotation.eulerAngles.y;
+            }
+            /*else
+            {
+                targetRotation = Mathf.Lerp(targetRotation, 0, 1.6f * mainModule.PersonalDeltaTime);
+                targetRotation = 0;
+            }*/
+
             rotation = Mathf.SmoothDampAngle(_rotate.y, targetRotation, ref rotationVelocity,
                 1.6f * mainModule.PersonalDeltaTime);
 
