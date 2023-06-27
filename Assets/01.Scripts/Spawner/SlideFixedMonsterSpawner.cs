dc036a45372a8af5f13d54ee31dc8fc8fc86f7c0
StraightBet <slupgames1025@gmail.com>
2023-06-28 00:53:23 +0900

    0628_스포너 구상

---
+++ Assets/01.Scripts/Spawner/SlideFixedMonsterSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spawner
+{
+    public class SlideFixedMonsterSpawner : MonoBehaviour
+    {
+        //몬스터SO 혹은 몬스터 어드레스
+        
+        //RandomMonsterSpawner의 소환 함수
+        
+        //Start함수를 만든다.
+        //Start함수에 소환 함수를 넣는다.
+        
+        
+    }   
+}
---
+++ Assets/01.Scripts/Spawner/SlideRandomMonsterSpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spawner
+{
+    public class SlideRandomMonsterSpawner : MonoBehaviour
+    {
+        //풀 관련
+        //퍼센트 배열
+        //랜덤몬스터SO 배열
+        
+        //함수 만들고 이걸 슬라이드 디텍트의 이벤트로 넘겨준다.
+        
+    }
+}
+
