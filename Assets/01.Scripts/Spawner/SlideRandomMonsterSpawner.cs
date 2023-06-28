fe2732721913893af03290a5b57b8c5e35028c6f
moonkanghyuck <moonkanghyuck@gmail.com>
2023-06-28 18:36:20 +0900

    0628_바위 밀치기 작업중

--- Assets/01.Scripts/Spawner/SlideRandomMonsterSpawner.cs
+++ Assets/01.Scripts/Spawner/SlideRandomMonsterSpawner.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pool;
+using Streaming;
+using Module;
+using Effect;
+using Utill.Random;
 
 namespace Spawner
 {
@@ -8,9 +13,67 @@ namespace Spawner
     {
         //풀 관련
         //퍼센트 배열
+        [SerializeField]
+        private float[] percentArray;
         //랜덤몬스터SO 배열
+        [SerializeField]
+        private RandomMonsterListSO[] monsterListSOList;
+        
+        [SerializeField]
+        private string spawnEffectAddress;
+
         
         //함수 만들고 이걸 슬라이드 디텍트의 이벤트로 넘겨준다.
+        public void SpawnRandomMonsterFromRandomListSO()
+        {
+            int _index = StaticRandom.Choose(percentArray);
+            RandomAndChoiceSpawn(monsterListSOList[_index]);
+        }
+        
+        
+        private void RandomAndChoiceSpawn(RandomMonsterListSO _randomMonsterListSO)
+        {
+            int _randomRange = Random.Range(_randomMonsterListSO.minSpawnCount, _randomMonsterListSO.maxSpawnCount + 1);
+            for(;_randomRange-- > 0 ; )
+            {
+                int _index = StaticRandom.Choose(_randomMonsterListSO.randomPercentArr);
+                RandomSpawn(_randomMonsterListSO.spawnMonsterDataArr[_index]);
+            }
+        }
+
+        private void RandomSpawn(RandomMonsterData _randomMonsterData)
+        {
+            int _randomRange = Random.Range(_randomMonsterData.minSpawnCount, _randomMonsterData.maxSpawnCount + 1);
+            for(; _randomRange-- > 0; )
+            {
+                StartCoroutine(Spawn(_randomMonsterData));
+            }
+        }
+        private IEnumerator Spawn(RandomMonsterData _randomMonsterData)
+        {
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+            Vector3 _spawnPos = transform.position;
+            GameObject obj = ObjectPoolManager.Instance.GetObject(_randomMonsterData.enemyAddress);
+            ObjectClassCycle objectClassCycle = obj.GetComponentInChildren<ObjectClassCycle>();
+            objectClassCycle.TargetObject = obj;
+            ObjectSceneChecker _objectSceneChecker = ClassPoolManager.Instance.GetClass<ObjectSceneChecker>();
+            if (_objectSceneChecker is null)
+            {
+                _objectSceneChecker = new ObjectSceneChecker();
+            }
+            _objectSceneChecker.ObjectDataSO = _randomMonsterData.objectDataSO;
+            _objectSceneChecker.ObjectClassCycle = objectClassCycle;
+            objectClassCycle.AddObjectClass(_objectSceneChecker);
+            
+            obj.transform.position = _spawnPos;
+            var _module = obj.GetComponent<AbMainModule>();
+            _module.attackedTime = 0f;
+            _module.knockBackVector = Vector3.up;
+            _module.knockBackPower = 15;
+            EffectManager.Instance.SetEffectDefault(spawnEffectAddress, _spawnPos, Quaternion.identity);
+			
+            obj.SetActive(true);
+        }
         
     }
 }
