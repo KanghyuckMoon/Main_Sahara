6500d87ee6971a9bbc5f22d8e12c8969eebc4c56
moonkanghyuck <moonkanghyuck@gmail.com>
2023-08-18 00:18:19 +0900

    0818_비석 제작

---
+++ Assets/01.Scripts/Talk/TalkObject.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utill.Measurement;
+using Quest;
+using Module;
+using Utill.Addressable;
+using Module.Shop;
+using Shop;
+using UI.PublicManager;
+using Pool;
+using System.Diagnostics;
+using System;
+
+namespace Module.Talk
+{
+	public class TalkObject : MonoBehaviour
+	{
+		public bool IsEndTalk
+		{
+			get
+			{
+				return isEndTalk;
+			}
+			set
+			{
+				isEndTalk = value;
+			}
+		}
+
+		[SerializeField]
+		private string talkCode;
+		[SerializeField]
+		private string authorCode;
+
+		private int index = 0;
+
+		public bool IsCanTalk
+		{
+			get
+			{
+				return isCanTalk;
+			}
+			set
+			{
+				isCanTalk = value;
+			}
+		}
+
+		private bool isFirst = false;
+		private bool isEndTalk = false;
+		private bool isTalking = false;
+
+		private bool isCanTalk = true;
+
+		public void Talk()
+		{
+			if (!isCanTalk)
+			{
+				return;
+			}
+			if (!isTalking)
+			{
+				Logging.Log("대화 가능");
+
+				//대화
+				GetText();
+				isEndTalk = false;
+				isTalking = true;
+			}
+		}
+
+		private void GetText()
+		{
+			PublicUIManager.Instance.SetTexts(authorCode, talkCode, EndTalk);
+			isTalking = true;
+		}
+
+		private void EndTalk()
+		{
+			isEndTalk = true;
+			isTalking = false;
+		}
+	}
+}
