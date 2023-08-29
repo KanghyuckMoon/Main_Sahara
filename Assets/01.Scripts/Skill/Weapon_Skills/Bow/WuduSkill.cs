ceb7fb103c28a03aca660c4f47d0c75054954d47
meng-joo <81345410+meng-joo@users.noreply.github.com>
2023-08-29 23:57:26 +0900

    0819_스킬_활 스킬 만드는 중

--- Assets/01.Scripts/Skill/Weapon_Skills/Bow/WuduSkill.cs
+++ Assets/01.Scripts/Skill/Weapon_Skills/Bow/WuduSkill.cs
@@ -1,16 +1,15 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using Module;
 using UnityEngine;
 using HitBox;
-using DG.Tweening;
 using Pool;
 using Utill.Addressable;
 
 namespace Skill
 {
-    public class Sw_00_ChippedSword_Skill : WeaponSkillFunctions, IWeaponSkill
+    public class WuduSkill : WeaponSkillFunctions, IWeaponSkill
     {
         [SerializeField]
         private AnimationClip animationClip;
@@ -18,28 +17,14 @@ namespace Skill
         [SerializeField] private HitBoxInAction hitBoxInAction;
         [SerializeField] private HitBoxAction hitBoxAction = new HitBoxAction();
 
-        [SerializeField] private string _skillEffectName;
-
-        private GameObject effect;
-
-        private void Start()
-        {
-            hitBoxAction.SetCondition(Hit, 30, HitBoxActionType.Hit);
-            hitBoxAction.SetCondition(HitEffect, Vector2.zero, HitBoxActionType.Start);
-        }
-
         public void Skills(AbMainModule _mainModule)
         {
-            UseMana(_mainModule, -usingMana);
+            
+            
             PlaySkillAnimation(_mainModule, animationClip);
-
-            //effect = ObjectPoolManager.Instance.GetObject(_skillEffectName);
-            //effect.transform.SetParent(transform);
-            //effect.transform.localPosition = Vector3.forward * 3;
-            //effect.SetActive(true);
-            //Invoke(nameof(SetEffectOff), 1.96f);
+            UseMana(_mainModule, usingMana);
+            GetBuff(_mainModule);
         }
-
         public HitBoxAction GetHitBoxAction()
         {
             return hitBoxAction;
@@ -55,12 +40,6 @@ namespace Skill
             //GameObject obj = ObjectPoolManager.Instance.GetObject("FireEffect_1");
             //obj.SetActive(true);
             //obj.transform.position = transform.position;
-        }
-
-        private void SetEffectOff()
-        {
-            effect.SetActive(false);
-            ObjectPoolManager.Instance.RegisterObject(_skillEffectName, effect);
         }
     }
 }
