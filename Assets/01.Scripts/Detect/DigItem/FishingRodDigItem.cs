7f15de2c047651353feb22401e2b348b42dfb407
moonkanghyuck <moonkanghyuck@gmail.com>
2023-08-29 15:33:54 +0900

    0829_낚시대 이펙트 수정

--- Assets/01.Scripts/Detect/DigItem/FishingRodDigItem.cs
+++ Assets/01.Scripts/Detect/DigItem/FishingRodDigItem.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using Effect;
 
 namespace Detect
 {
@@ -9,8 +11,10 @@ public class FishingRodDigItem : BaseDigItem
 {
     [SerializeField] private string animString;
     [SerializeField] private Animator animator;
-    
-    protected override void OnEnable()
+	[SerializeField] private Transform boat;
+        private bool isFindAndOut;
+
+		protected override void OnEnable()
     {
         var _detectAnimationAction = transform.root.GetComponentInParent<DetectAnimationAction>();
         if (_detectAnimationAction is not null)
@@ -41,6 +45,56 @@ public class FishingRodDigItem : BaseDigItem
             playerAnimator.Play(animString);
         }
     }
-}
+
+    public void BoatStart()
+    {
+			int floorLayerIndex = LayerMask.NameToLayer("Ground");
+			int layerMask = (1 << floorLayerIndex);
+            Vector3 forward = transform.root.forward;
+            forward.y = 0; 
+			Vector3 pos = transform.position + forward.normalized * 5 + Vector3.up * 10;
+            RaycastHit hit;
+            if(Physics.Raycast(pos, Vector3.down, out hit, 30, layerMask))
+			{
+				boat.DOJump(hit.point + Vector3.down * 0.1f, 6f, 1, 0.6f).OnComplete(() =>
+				{
+					GetNearObject();
+					EffectManager.Instance.SetEffectDefault("SmallBoomSandVFX", hit.point, Quaternion.identity);
+					if (targetItem is not null)
+					{
+						EffectManager.Instance.SetEffectDefault("FishingAreaFindEffect", hit.point, Quaternion.identity);
+                        isFindAndOut = true;
+					}
+					else
+					{
+						EffectManager.Instance.SetEffectDefault("FishingAreaEffect", hit.point, Quaternion.identity);
+                        isFindAndOut = false;
+					}
+                });
+			}
+            else
+            {
+                pos += Vector3.down * 13;
+				boat.DOJump(pos, 5f, 1, 0.6f);
+                isFindAndOut = false;
+			}
+	}
+
+
+    public void BoatEnd()
+		{
+            if(isFindAndOut)
+            {
+			    EffectManager.Instance.SetEffectDefault("BoomSandVFX", boat.position, Quaternion.identity);
+            }
+            else
+			{
+				EffectManager.Instance.SetEffectDefault("SmallBoomSandVFX", boat.position, Quaternion.identity);
+			}
+
+			boat.DOLocalJump(new Vector3(-0.3f, 2.4f, 0.23f), 2f, 1, 0.4f);
+		}
+
+	}
 
 }
