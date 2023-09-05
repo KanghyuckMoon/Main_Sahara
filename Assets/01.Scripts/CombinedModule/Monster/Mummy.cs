724b3cf5f9be516e824b32b840ffacbfe392c130
moonkanghyuck <moonkanghyuck@gmail.com>
2023-09-06 01:22:07 +0900

    0906_미라쪽 수정

--- Assets/01.Scripts/CombinedModule/Monster/Mummy.cs
+++ Assets/01.Scripts/CombinedModule/Monster/Mummy.cs
@@ -50,7 +50,7 @@ namespace CondinedModule
             AddModuleWithPool<JumpModule>(ModuleType.Jump);
             AddModuleWithPool<HpModule>(ModuleType.Hp);
             AddModuleWithPool<AnimationModule>(ModuleType.Animation);
-            AddModuleWithPool<PhysicsModule>(ModuleType.Physics);
+            AddModuleWithPool<PhysicsRotateHillModule>(ModuleType.Physics);
             switch (hudType)
             {
                 case HUDType.Default:
