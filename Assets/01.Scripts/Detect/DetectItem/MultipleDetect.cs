600fe2def2181b8237fc2b8b0026323b91c43d1b
moonkanghyuck <moonkanghyuck@gmail.com>
2023-09-11 20:29:40 +0900

    0911_본편 맵 수정중

---
+++ Assets/01.Scripts/Detect/DetectItem/MultipleDetect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Detect
+{
+	public class MultipleDetect : MonoBehaviour
+	{
+		[SerializeField] private List<BaseDetectItem> baseDetectItems = new List<BaseDetectItem>();
+
+		private void Start()
+		{
+			foreach (var item in baseDetectItems)
+			{
+				item.getoutEventBefore.AddListener(AllGetOut);
+			}
+		}
+
+		public void AllGetOut()
+		{
+			foreach(var item in baseDetectItems)
+			{
+				item.GetOut();
+			}
+		}
+	}
+}
